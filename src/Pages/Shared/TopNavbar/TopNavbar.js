86cac9abb25c97ef65823a86df23e137de35ee98
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-12-29 13:14:36 +0600

    fix

--- src/Pages/Shared/TopNavbar/TopNavbar.js
+++ src/Pages/Shared/TopNavbar/TopNavbar.js
@@ -41,10 +41,10 @@ const TopNavbar = () => {
     <img
       src={user?.photoURL}
       className="mr-3 h-6 sm:h-9"
-      alt="Flowbite Logo"
+      alt="Logo"
     />
     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-      Flowbite
+      TMS
     </span>
   </Navbar.Brand>
   <Navbar.Toggle />
